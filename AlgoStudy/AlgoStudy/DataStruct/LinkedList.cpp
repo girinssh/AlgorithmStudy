548b162f914e5e2515c84f1d9d361139c3466f25
girinssh <tngus5851@naver.com>
2019-03-19 00:18:12 +0900

    DataStruct_Commit

--- AlgoStudy/AlgoStudy/DataStruct/LinkedList.cpp
+++ AlgoStudy/AlgoStudy/DataStruct/LinkedList.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
-class List {
+template<typename T> class  List {
+	T Data;
+	List * next;
+
+	List<T>() {
+		Data = NULL;
+		next = NULL;
+	}
+	List<T>(T data) {
+		Data = data;
+		next = NULL;
+	}
 
 };
